dd051ca79d275503f0ec92ce7a786e18ac4f9d9f
Adithya Viswanathan <adithya.viswanathan26@gmail.com>
2024-03-07 12:04:24 -0600

    test query works

--- sql.py
+++ sql.py
@@ -10,9 +10,16 @@ with open("secrets.json", "r") as file:
     secrets = json.load(file)
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user=secrets['SQL_USERNAME'],
-  password=secrets["SQL_PASSWORD"]
+  host = "localhost",
+  user = secrets['SQL_USERNAME'],
+  password = secrets["SQL_PASSWORD"],
+  database = secrets["DATABASE_NAME"],
 )
 
-print(mydb)
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT N_NATIONKEY, N_Name FROM NATION WHERE N_Name LIKE '%AN%'")
+
+myresult = mycursor.fetchall()
+
+print(myresult)
